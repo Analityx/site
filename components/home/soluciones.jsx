600813d8b8f4f78a3562e3fc91e4ad7e39486306
Amin-farina <amiin74@gmail.com>
2024-04-17 12:55:57 -0300

    Fix Solucions

--- components/home/soluciones.jsx
+++ components/home/soluciones.jsx
@@ -3,6 +3,7 @@ import { useRef } from "react";
 import { VerticalCard } from "./verticalCard";
 import { useScroll } from "framer-motion"
 import { Reveal } from "../utils/reveal";
+import "@/app/globals.css"
 
 export function Soluciones() {
     const ref = useRef(null)
@@ -28,13 +29,13 @@ export function Soluciones() {
         <section className="my-[2vh] " ref={ref}>
             <div className="md:ms-5 lg:ms-5 w-full text-center">
                 <Reveal>
-                    <h1 className="text-5xl max-sm:ps-4 text-[#474747]">Soluciones</h1>
+                    <h1 className="text-5xl max-sm:ps-4 text-[#474747] font-aspekta">Soluciones</h1>
                 </Reveal>
             </div>
             <div className="w-full flex max-sm:flex-wrap justify-center" style={{ pathLength: scrollYProgress }}>
-                <div className="w-full max-sm:flex-wrap max-sm:w-full flex justify-center">
-                    {soluciones.map((element) => (
-                        <VerticalCard key={element.title} imageSrc={element.imgSrc} title={element.title} description={element.description} href={element.href} />
+                <div className="w-full flex-wrap max-sm:w-full flex justify-center">
+                    {soluciones.map((element, index) => (
+                        <VerticalCard key={element.title} imageSrc={element.imgSrc} title={element.title} description={element.description} href={element.href} index={index} />
                     ))}
                 </div>
             </div>
--- components/home/verticalCard.jsx
+++ components/home/verticalCard.jsx
@@ -1,31 +1,44 @@
 import Image from "next/image";
 import { Reveal } from "../utils/reveal";
 import Link from "next/link";
+import "@/app/globals.css"
 
-export const VerticalCard = ({ imageSrc, title, description, href }) => {
+export const VerticalCard = ({ imageSrc, title, description, href, index }) => {
     return (
-        <div className="md:mx-3 lg:mx-3 max-sm:px-3 w-1/2 max-sm:w-full my-[2vh] max-sm:my-3 flex justify-center items-center">
-            <div className=" lg:w-[75%] md:w-[100%] bg-[#1EA131] rounded-xl overflow-hidden shadow-md mb-4 py-4 px-5">
-                <Reveal>
-                    <Image src={imageSrc} alt={title} className="w-full rounded-[20px]  object-fill" width={500} height={500} />
-                </Reveal>
-                <div className="pt-6 text-white p-2 my-5">
+        <div className="md:mx-3 lg:mx-3 max-sm:px-3 w-full my-[2vh] max-sm:my-3 flex justify-center items-center">
+            <div className={` w-full flex ${index === 1 ? ("flex-row-reverse max-sm:flex-col-reverse flex-wrap-reverse") : ("max-sm:flex-col-reverse flex-wrap")} rounded-xl overflow-hidden shadow-md mb-4 py-4 px-5`}>
+                <div className="w-1/2 max-sm:w-full px-2 ">
                     <Reveal>
-                        <h2 className="text-2xl font-bold mb-2">{title}</h2>
-                    </Reveal>
-                    <Reveal>
-                        <p className="mb-4 font-normal ">{description}</p>
-                    </Reveal>
-                    <Reveal>
-                        <div className="max-sm:min-h-[7vh] relative">
-                            <Link href={href} className="bg-transparent border-2 my-2 font-bold border-white rounded-[20px] text-white px-4 py-2 hover:bg-white hover:text-black absolute">
-                                Conocé más
+                        <div className=" overflow-hidden max-h-[45vh] rounded-2xl">
+                            <Image src={imageSrc} alt={title} className="object-cover" width={1920} height={1080} />
+                        </div>
+                        <div className="md:hidden xl:hidden 2xl:hidden lg:hidden w-full flex pt-3 justify-center">
+                            <Link href={href} className=" bg-[#0A09B4] text-white border-2 rounded-[20px] font-bold border-white  px-4 py-2 hover:bg-[#1EA932]">
+                                <button className="px-4 ">Conoce Más</button>
                             </Link>
                         </div>
                     </Reveal>
-
-
                 </div>
+                <div className="my-5 px-4 w-1/2 max-sm:w-full flex flex-wrap justify-center items-center">
+                    <div className="w-full">
+                        <Reveal>
+                            <h2 className="2xl:text-4xl lg:text-3xl font-aspekta max-sm:pb-2 pb-5 text-2xl text-center font-bold mb-2 text-[#474747]">{title}</h2>
+                        </Reveal>
+                        <Reveal>
+                            <div className="w-full flex justify-center ">
+                                <div className="w-[80%] max-sm:w-full">
+                                    <p className="mb-4 max-sm:mb-2 font-arial ">{description}</p>
+                                </div>
+                            </div>
+                            <div className="max-sm:hidden w-full flex pt-3 justify-center">
+                                <Link href={href} className=" bg-[#0A09B4] text-white border-2 rounded-[20px] font-bold border-white  px-4 py-2 hover:bg-[#1EA932]">
+                                    <button className="px-4 ">Conoce Más</button>
+                                </Link>
+                            </div>
+                        </Reveal>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
