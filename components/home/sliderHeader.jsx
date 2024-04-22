01b659f544daa8a0b0a1737e182bebc1f50d8dde
Amin-Farina <58991019+amin-farina@users.noreply.github.com>
2024-04-22 20:28:15 -0300

    Update sliderHeader.jsx

--- components/home/sliderHeader.jsx
+++ components/home/sliderHeader.jsx
@@ -40,7 +40,7 @@ const Slider = () => {
                 initial={{ opacity: 0, x: -200 }}
                 animate={{ opacity: 1, x: 0, }}
                 transition={{ duration: 0.5, ease: "easeIn", delay: 0.25 }} className="w-full">
-                <h1 className="title-header min-2xl:text-[7em] min-xl:text-[5em] min-md:text-[3em] text-left text-white w-1/2  max-sm:text-[2.5em] uppercase leading-[0.9] max-sm:leading-8 tracking-tighter">
+                <h1 className="title-header max-2xl:text-[7em] max-xl:text-[5em] max-md:text-[3em] text-left text-white w-1/2  max-sm:text-[2.5em] uppercase leading-[0.9] max-sm:leading-8 tracking-tighter">
                     {slides[currentSlide].title}
                 </h1>
             </motion.div>
