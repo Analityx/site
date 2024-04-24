f17dd34b4f812da945d9b38e4a585c1d37d5e7cf
Amin-farina <amiin74@gmail.com>
2024-04-24 12:49:11 -0300

    Nuevos y ultimos cambios

---
+++ app/loading.jsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Loading() {
+    return (
+        <section className="bg-white w-[100vw] h-[100vh] flex justify-center items-center z-[99]">
+            <img src="/assets/img/analityx-logo-color.png" alt="Logo analityx" className="w-full h-auto" />
+        </section>
+    )
+}
